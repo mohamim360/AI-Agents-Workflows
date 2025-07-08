f41024c848e9026439e307ca36977ef8e1e199a6
mohamim360 <mohamim360@gmail.com>
2025-07-08 06:32:44 +0600

    Getting Started With A First Example Workflow

--- Essentials/main.py
+++ Essentials/main.py
@@ -1,5 +1,14 @@
+def generate_x_post(usr_input: str) -> str:
+    #call AI /LLM
+    pass
+
 def main():
-    print("Hello from essentials!")
+    # user input => AI (LLM) to generate X post => output post
+    usr_input = input("What should the post be about? ")
+    x_post = generate_x_post(usr_input)
+    print("Generate X post:")
+    print(x_post)
+
 
 
 if __name__ == "__main__":
